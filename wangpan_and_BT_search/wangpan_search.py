141122e16948544b1ac9f301507cec0411a3d634
egrcc <zhaolujun1994@gmail.com>
2014-12-25 11:29:00 +0800

    windows

--- wangpan_and_BT_search/wangpan_search.py
+++ wangpan_and_BT_search/wangpan_search.py
@@ -12,6 +12,7 @@
 
 import re
 import WangPan
+import platform
 import requests
 from bs4 import BeautifulSoup
 
@@ -30,8 +31,12 @@ def search(keywords, start_page = 1, end_page = 1, wangpan = WangPan.All):
 
         if len(result_list) == 0:
             print "*********************************************"
-            print "对不起，这个真没有！"
-            print "虽然已经很努力了，但还是没能找到您想要的结果。"
+            if platform.system() == "Windows":
+                print "对不起，这个真没有！".decode("utf-8").encode("gbk")
+                print "虽然已经很努力了，但还是没能找到您想要的结果。".decode("utf-8").encode("gbk")
+            else:
+                print "对不起，这个真没有！"
+                print "虽然已经很努力了，但还是没能找到您想要的结果。"
             print "*********************************************", '\n'
 
         url_and_content_list = []
@@ -54,30 +59,59 @@ def search(keywords, start_page = 1, end_page = 1, wangpan = WangPan.All):
         current_page = current_page + 1
 
 
-print '''#########################################################
-# 网盘搜索脚本，可搜索国内各大网盘资源
-# 使用到的第三方库：requests，BeautifulSoup4
-# 本程序使用网盘搜（http://www.wangpansou.cn/）的搜索结果
-# 经由程序搜索所产生的任何结果皆不代表本人立场
-# 本人不对其真实合法性以及版权负责，亦不承担任何法律责任
-# 使用方法：终端中输入 “python wangpan_search.py”运行
-# Author：egrcc
-# Email：zhaolujun1994@gmail.com
-# Version：0.1
-#########################################################'''
-kw = raw_input("请输入关键字（多个关键字请以空格隔开）:")
-keywords = kw.split()
-while True:
-    try:
-        page = int(raw_input("请输入搜索结果的显示页数（每页10条结果）:"))
-    except Exception:
-        print "输入有误，必须输入一个大于0的整数."
-        continue
-    else:
-        if page <= 0:
+if platform.system() == "Windows":
+    print '''#########################################################
+    # 网盘搜索脚本，可搜索国内各大网盘资源
+    # 使用到的第三方库：requests，BeautifulSoup4
+    # 本程序使用网盘搜（http://www.wangpansou.cn/）的搜索结果
+    # 经由程序搜索所产生的任何结果皆不代表本人立场
+    # 本人不对其真实合法性以及版权负责，亦不承担任何法律责任
+    # 使用方法：终端中输入 “python wangpan_search.py”运行
+    # Author：egrcc
+    # Email：zhaolujun1994@gmail.com
+    # Version：0.1
+    #########################################################'''.decode("utf-8").encode("gbk")
+else:
+    print '''#########################################################
+    # 网盘搜索脚本，可搜索国内各大网盘资源
+    # 使用到的第三方库：requests，BeautifulSoup4
+    # 本程序使用网盘搜（http://www.wangpansou.cn/）的搜索结果
+    # 经由程序搜索所产生的任何结果皆不代表本人立场
+    # 本人不对其真实合法性以及版权负责，亦不承担任何法律责任
+    # 使用方法：终端中输入 “python wangpan_search.py”运行
+    # Author：egrcc
+    # Email：zhaolujun1994@gmail.com
+    # Version：0.1
+    #########################################################'''
+if platform.system() == "Windows":
+    kw = raw_input("请输入关键字（多个关键字请以空格隔开）:".decode("utf-8").encode("gbk"))
+    kw = kw.decode("gbk").encode("utf-8")
+    keywords = kw.split()
+    while True:
+        try:
+            page = int(raw_input("请输入搜索结果的显示页数（每页10条结果）:".decode("utf-8").encode("gbk")))
+        except Exception:
+            print "输入有误，必须输入一个大于0的整数.".decode("utf-8").encode("gbk")
+            continue
+        else:
+            if page <= 0:
+                print "输入有误，必须输入一个大于0的整数.".decode("utf-8").encode("gbk")
+                continue
+        break
+else:
+    kw = raw_input("请输入关键字（多个关键字请以空格隔开）:")
+    keywords = kw.split()
+    while True:
+        try:
+            page = int(raw_input("请输入搜索结果的显示页数（每页10条结果）:"))
+        except Exception:
             print "输入有误，必须输入一个大于0的整数."
             continue
-    break
+        else:
+            if page <= 0:
+                print "输入有误，必须输入一个大于0的整数."
+                continue
+        break
 
 print ''
 search(keywords, end_page = page)
